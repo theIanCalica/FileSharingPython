501609b7da7ff0a9803654fc2138f9090ac25c4e
Ian Calica <Iggc654@gmail.com>
2024-10-31 00:14:09 +0800

    Updated serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -30,15 +30,8 @@ class UserSerializer(serializers.ModelSerializer):
 class FolderSerializer(serializers.ModelSerializer):
   class Meta:
     model = Folder
-    fields = ['name', 'owner', 'created_at']
-    
-  def create(self, validated_data):
-    folder = Folder(
-      name=validated_data['name'],
-      owner=validated_data['owner']
-    )
-    folder.save()
-    return folder
+    fields = ['name', 'created_at']
+    read_only_fields = ['id', 'created_at']
   
 class FileSerializer(serializers.ModelSerializer):
   class Meta:
--- backend/api/views.py
+++ backend/api/views.py
@@ -104,3 +104,25 @@ class FileViewSet(viewsets.ModelViewSet):
             return Response({"message": "File deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
         except File.DoesNotExist:
             return Response({"error": "File not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+class FolderViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.AllowAny]
+    queryset = Folder.objects.all()
+    serializer_class = FolderSerializer
+    
+    def get_queryset(self):
+        user_id = self.request.user.id  
+        return Folder.objects.filter(user_id=user_id)
+    
+    def perform_create(self, serializer):
+        # Automatically associates the folder with the authenticated user
+        serializer.save(user=self.request.user)
+        
+    def destroy(self, request, pk=None):
+        """Delete a folder by primary key"""
+        try:
+            folder = Folder.objects.get(pk=pk)
+            folder.delete()
+            return Response({"message": "User deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+        except Folder.DoesNotExist:
+            return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
