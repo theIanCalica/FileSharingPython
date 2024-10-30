e4a94239c919bf24a7b27cea7ff78ece3dc65988
Ian Calica <Iggc654@gmail.com>
2024-10-30 20:34:09 +0800

    Login and logout functions

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,12 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from django.http import HttpResponse
 from rest_framework import viewsets, permissions
 from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.response import Response
 from .serializers import *
+from .models import *
 from django.contrib.auth.models import User
 
+@api_view(["POST"])
+def login(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    
+    user = authenticate(request,username=username, password=password)
+    if user is not None:
+        # Logic for generating a token or session, if needed
+        return Response({"message": "Login successful"}, status=status.HTTP_200_OK)
+    else:
+        return Response({"error": "Invalid username or password"}, status=status.HTTP_401_UNAUTHORIZED)
+        
+
+@api_view(["POST"])
+def logout(request):
+    logout(request)
+    return Response({"message": "Logout successful"}, status=200)
+            
 class UserViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.AllowAny]
     queryset = User.objects.all()
@@ -56,3 +77,30 @@ class UserViewSet(viewsets.ModelViewSet):
             return Response({"message": "User deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
         except User.DoesNotExist:
             return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+class FileViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.AllowAny]
+    queryset = File.objects.all()
+    serializer_class = FileSerializer
+    
+    
+    def update(self, request, pk=None):
+        """Update an existing file"""
+        try: 
+            file = File.objects.get(pk=pk)
+        except File.DoesNotExist:
+            return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = self.get_serializer(file, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def destroy(self, request, pk=None):
+        """ Delete a file by primary key (pk). """
+        try:
+            file = File.objects.get(pk=pk)
+            file.delete()
+            return Response({"message": "File deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+        except File.DoesNotExist:
+            return Response({"error": "File not found"}, status=status.HTTP_404_NOT_FOUND)
