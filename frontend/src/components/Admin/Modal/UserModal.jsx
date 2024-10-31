e8a17e96718893443a5c80bcbed83400b8681317
Ian Calica <Iggc654@gmail.com>
2024-10-31 22:37:16 +0800

    Updated user modal and user page

--- frontend/src/components/Admin/Modal/UserModal.jsx
+++ frontend/src/components/Admin/Modal/UserModal.jsx
@@ -1,19 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { useForm, Controller } from "react-hook-form";
-import Select from "react-select";
-import TextField from "@mui/material/TextField";
+import { useForm } from "react-hook-form";
 import {
   notifyError,
   notifySuccess,
-  formatDate,
   getBorderColor,
 } from "../../../utils/Helpers";
-import { Box } from "@mui/material";
 import axios from "axios";
-import dayjs from "dayjs";
 
 const UserModal = ({
   onClose,
@@ -50,7 +42,6 @@ const UserModal = ({
   const {
     register,
     handleSubmit,
-    control,
     reset,
     setError,
     formState: { errors, touchedFields },
@@ -60,9 +51,6 @@ const UserModal = ({
       fname: "",
       lname: "",
       email: "",
-      phoneNumber: "",
-      dob: null,
-      role: null,
       password: "",
     },
   });
@@ -74,24 +62,15 @@ const UserModal = ({
         fname: userToEdit.fname,
         lname: userToEdit.lname,
         email: userToEdit.email,
-        phoneNumber: userToEdit.phoneNumber,
-        role: options.find((option) => option.value === userToEdit.role),
-        dob: userToEdit.dob ? dayjs(userToEdit.dob) : null,
       });
     }
   }, [isEditing, userToEdit, reset]);
 
   const [isEmailUnique, setIsEmailUnique] = useState(true);
-  const [isPhoneNumberUnique, setIsPhoneNumberUnique] = useState(true);
-
-  const options = [
-    { value: "customer", label: "Customer" },
-    { value: "serviceCrew", label: "Service Crew" },
-  ];
 
   const onSubmit = (data) => {
-    if (!isEmailUnique || !isPhoneNumberUnique) {
-      notifyError("Email or Phone Number must be unique");
+    if (!isEmailUnique) {
+      notifyError("Email already in use");
       return;
     }
 
@@ -99,11 +78,7 @@ const UserModal = ({
       fname: data.fname,
       lname: data.lname,
       email: data.email,
-      phoneNumber: data.phoneNumber,
-      role: data.role.value,
-      dob: data.dob.toISOString(),
       password: data.password,
-      status: "activated",
     };
 
     console.log(user);
@@ -187,54 +162,6 @@ const UserModal = ({
               </p>
             )}
           </div>
-          {/* Date of Birth */}
-          <div className="mb-4">
-            <label htmlFor="dob" className="block text-gray-700 mb-2">
-              Date of Birth
-            </label>
-            <Controller
-              name="dob"
-              rules={{ required: "Date of Birth is required" }}
-              control={control}
-              render={({ field }) => (
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <div className="w-full">
-                    <Box
-                      sx={{
-                        width: "100%", // Ensure Box takes full width of its container
-                        "& .MuiOutlinedInput-root": {
-                          borderRadius: "8px", // Custom border radius
-                          width: "100%",
-                          "& fieldset": {
-                            borderColor: errors.dob ? "#f87171" : "#e5e7eb", // Conditional border color
-                          },
-                          "&:hover fieldset": {
-                            borderColor: errors.dob ? "#f87171" : "#0056b3", // Conditional border color on hover
-                          },
-                        },
-                      }}
-                    >
-                      <DatePicker
-                        {...field}
-                        className="MuiDayCalendar-header MuiDayCalendar-weekContainer"
-                        renderInput={(params) => (
-                          <TextField
-                            {...params}
-                            fullWidth // Make the TextField take up all available space
-                            onBlur={() => field.onBlur()}
-                          />
-                        )}
-                      />
-                    </Box>
-                  </div>
-                </LocalizationProvider>
-              )}
-            />
-
-            {errors.dob && (
-              <p className="text-red-500 text-sm mt-1">{errors.dob.message}</p>
-            )}
-          </div>
 
           <div className="mb-4">
             <label htmlFor="email" className="block text-gray-700 mb-2">
@@ -278,122 +205,28 @@ const UserModal = ({
               </p>
             )}
           </div>
+
           <div className="mb-4">
-            <label htmlFor="phoneNumber" className="block text-gray-700 mb-2">
-              Phone Number
+            <label htmlFor="username" className="block text-gray-700 mb-2">
+              Username
             </label>
             <input
-              id="phoneNumber"
+              id="username"
               type="text"
               className={`w-full px-3 py-2 border border-gray-300 rounded-md h-14 ${getBorderColor(
-                "phoneNumber",
+                "username",
                 errors,
                 touchedFields
               )}`}
-              {...register("phoneNumber", {
-                required: "Phone Number is required",
-                pattern: {
-                  value: /^((09)|(\+639))\d{9}$/,
-                  message: "Invalid phone number format",
-                },
-                onChange: async (value) => {
-                  console.log(value);
-                },
-              })}
-              maxLength={11}
+              {...register("username", { required: "Username is required" })}
             />
-            {errors.phoneNumber && (
+            {errors.username && (
               <p className="text-red-500 text-sm mt-1">
-                {errors.phoneNumber.message}
+                {errors.username.message}
               </p>
             )}
           </div>
-          <div className="mb-4">
-            <label htmlFor="role" className="block text-gray-700 mb-2">
-              Role
-            </label>
-            <Controller
-              name="role"
-              control={control}
-              rules={{ required: "Role is required" }}
-              render={({ field }) => (
-                <Select
-                  placeholder={"Select a Role"}
-                  {...field}
-                  options={options}
-                  onChange={(selectedOption) => {
-                    field.onChange(selectedOption); // Trigger react-hook-form's onChange
-                    field.onBlur(); // Mark field as touched
-                  }}
-                  onBlur={() => field.onBlur()} // Mark field as touched
-                  className={`w-full ${getBorderColor(
-                    "select",
-                    errors,
-                    touchedFields
-                  )}`}
-                  isClearable
-                  styles={{
-                    control: (provided, state) => ({
-                      ...provided,
-                      height: "3.5rem", // Adjust height here
-                      minHeight: "3.5rem", // Ensure minimum height
-                      borderColor: errors.role
-                        ? "#f87171"
-                        : provided.borderColor,
-                      boxShadow: state.isFocused
-                        ? "0 0 0 1px #4ade80"
-                        : provided.boxShadow,
-                    }),
-                    valueContainer: (provided) => ({
-                      ...provided,
-                      height: "100%", // Align container height
-                      padding: "0 0.75rem", // Adjust padding
-                    }),
-                    singleValue: (provided) => ({
-                      ...provided,
-                      height: "100%", // Align single value height
-                      lineHeight: "3.5rem", // Center text vertically
-                    }),
-                    indicator: (provided) => ({
-                      ...provided,
-                      height: "100%", // Align indicator height
-                      padding: "0", // Remove extra padding
-                    }),
-                    indicatorsContainer: (provided) => ({
-                      ...provided,
-                      height: "100%", // Align indicators container height
-                    }),
-                    menu: (provided) => ({
-                      ...provided,
-                      zIndex: 9999, // Ensure the dropdown is on top of other elements
-                    }),
-                    option: (provided, state) => ({
-                      ...provided,
-                      textAlign: "left", // Align text to the left
-                      display: "flex",
-                      alignItems: "center", // Center text vertically
-                      backgroundColor: state.isSelected
-                        ? "#4ade80"
-                        : provided.backgroundColor, // Tailwind color
-                      color: state.isSelected ? "#fff" : provided.color,
-                      "&:hover": {
-                        backgroundColor: "#d1d5db", // Tailwind color on hover
-                      },
-                    }),
-                    placeholder: (provided) => ({
-                      ...provided,
-                      color: "#9ca3af", // Tailwind text-gray-400 color
-                    }),
-                  }}
-                />
-              )}
-            />
-            {errors.role && (
-              <p className="text-red-500 text-sm mt-1">{errors.role.message}</p>
-            )}
-          </div>
           <div className="mb-4 col-span-2">
-            {" "}
             {/* Use col-span-2 to take both columns */}
             <label htmlFor="password" className="block text-gray-700 mb-2">
               Password
